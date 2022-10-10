9d637e4706b42c292fad978052c19310006a4e50
Moise-code <moisewowe@gmail.com>
2022-10-10 20:04:10 +0200

    Add javascript codes to mobile menu

---
+++ main.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click',(e)=>{
+    e.preventDefault();
+    hamburger.classList.toggle('active')
+    menu.classList.toggle('active')
+})
+
+document.querySelectorAll('.nav-link').forEach(nav => nav.addEventListener('click',() => {
+
+ hamburger.classList.remove('active');
+ menu.classList.remove('active');
+
+}))
